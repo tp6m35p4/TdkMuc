60921f75cc9b5659db2d9a0e980c40368743d21e
tp6m35p4 <tp6m35p4@gmail.com>
2017-09-30 15:36:39 +0800

    1.2.1
    capture frame

--- src/ian/main/mcu/CyzClass.java
+++ src/ian/main/mcu/CyzClass.java
@@ -1,15 +1,52 @@
 package ian.main.mcu;
 
 import ian.main.MainStart;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
+
 
 public class CyzClass {
 	
 	static MwcData info = MainStart.info;
 	static MwcSetData setRc = MainStart.setRc;
 	
+	private static Mat cap() {
+		
+		Core.setErrorVerbosity(false);
+        VideoCapture camera = new VideoCapture(0);
+        camera.set(Videoio.CV_CAP_PROP_FRAME_WIDTH, 500);
+        camera.set(Videoio.CV_CAP_PROP_FRAME_HEIGHT, 500);
+        try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        if(!camera.isOpened()){
+            System.out.println("Camera Error");
+        }
+        else {
+            System.out.println("Camera OK");
+        }
+ 
+        Mat frame = new Mat();
+        camera.read(frame);
+		
+		
+		return frame;
+	}
+	
 	public static void mode() {
 		
-		
+		Mat f = cap();
+		Mat grayImage = new Mat();
+		Mat detectedEdges = new Mat();
+		Imgproc.cvtColor(f, grayImage, Imgproc.COLOR_BGR2GRAY);
+		Imgproc.blur(grayImage, detectedEdges, new Size(3, 3));
 		
 	}
 }
